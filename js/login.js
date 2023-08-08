05e0eca332a3897ded8ecafe544d5a0583d8f1ec
Jatin Kushwah <singhjatin354@gmail.com>
2023-08-08 13:43:11 +0530

    login, signup authentication added

---
+++ js/login.js
@@ -0,0 +1,40 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDzoJJ_325VL_axuuAFzDf3Bwt_ENzu2rM",
+    authDomain: "jobsdoor360-39b87.firebaseapp.com",
+    databaseURL: "https://jobsdoor360-39b87-default-rtdb.firebaseio.com",
+    projectId: "jobsdoor360-39b87",
+    storageBucket: "jobsdoor360-39b87.appspot.com",
+    messagingSenderId: "326416618185",
+    appId: "1:326416618185:web:de19e90fe4f06006ef3318",
+    measurementId: "G-60RHEMJNM6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const loginForm = document.getElementById("login-form");
+
+loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        await loginUser(email, password);
+        window.location.href = "../index.html";
+    } catch (error) {
+        alert("Error logging in: " + error.message);
+    }
+});
+
+async function loginUser(email, password) {
+    const auth = getAuth();
+    await signInWithEmailAndPassword(auth, email, password);
+}
