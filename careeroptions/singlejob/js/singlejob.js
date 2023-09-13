207f4b83f4fec63954f3bd676edda05dd01af6cf
Jatin Kushwah <singhjatin354@gmail.com>
2023-09-13 18:05:56 +0530

    Profile analyzer updated.

--- careeroptions/singlejob/js/singlejob.js
+++ careeroptions/singlejob/js/singlejob.js
@@ -28,25 +28,112 @@ function getParameterByName(name) {
 }
 
 // Extract the 'jobCode' from URL params
-const jobCode = getParameterByName("jobCode");
+const jobCode = decodeURIComponent(getParameterByName("jobCode"));
+
+// Function to update element text content or hide it if it's empty
+function updateElementHTMLOrHide(elementId, html) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        if (html) {
+            element.innerHTML = html;
+            element.style.display = "block";
+        } else {
+            element.style.display = "none";
+        }
+    }
+}
+
+// Function to fetch job data from local storage by jobCode
+function fetchJobDataFromLocalStorage(jobcode) {
+    const jobsDataFromLocalStorage = JSON.parse(
+        localStorage.getItem("jobsData")
+    );
+
+    if (!jobsDataFromLocalStorage) {
+        console.error("Jobs data not found in local storage.");
+        return null;
+    }
+
+    const job = jobsDataFromLocalStorage.find(
+        (job) => decodeURIComponent(job.jobCode) === jobcode
+    );
+    return job || null;
+}
 
 async function fetchJobDocument(jobCode) {
     try {
-        const jobCodeNumber = parseInt(jobCode);
         const querySnapshot = await getDocs(
-            query(
-                collection(db, "jobs"),
-                where("job_code", "==", jobCodeNumber)
-            )
+            query(collection(db, "jobs"), where("job_code", "==", jobCode))
         );
 
         if (!querySnapshot.empty) {
-            return querySnapshot.docs[0].data();
-        } else {
-            console.error(
-                "No matching document found for jobCode:",
-                jobCodeNumber
+            const job = querySnapshot.docs[0].data();
+            const postName = job.post_name;
+
+            // Fetch the "posts" document using "post_name"
+            const postQuerySnapshot = await getDocs(
+                query(
+                    collection(db, "posts"),
+                    where("post_name", "==", postName)
+                )
             );
+
+            if (!postQuerySnapshot.empty) {
+                const postData = postQuerySnapshot.docs[0].data();
+
+                // Fetch "industry_masterdata", "jobtype_masterdata", and "profile_masterdata"
+                const industryCode = postData.industry_masterdata_code;
+                const jobTypeCode = postData.jobtype_masterdata_code;
+                const profileCode = postData.profile_masterdata_code;
+
+                // Fetch data from "industry_masterdata"
+                const industryQuerySnapshot = await getDocs(
+                    query(
+                        collection(db, "industry_masterdata "),
+                        where("code", "==", industryCode)
+                    )
+                );
+
+                const industry =
+                    industryQuerySnapshot.docs[0]?.data()?.name || "";
+
+                // Fetch data from "jobtype_masterdata"
+                const jobTypeQuerySnapshot = await getDocs(
+                    query(
+                        collection(db, "jobtype_masterdata"),
+                        where("code", "==", jobTypeCode)
+                    )
+                );
+
+                const jobType =
+                    jobTypeQuerySnapshot.docs[0]?.data()?.name || "";
+
+                // Fetch data from "profile_masterdata"
+                const profileQuerySnapshot = await getDocs(
+                    query(
+                        collection(db, "profile_masterdata "),
+                        where("code", "==", profileCode)
+                    )
+                );
+
+                const profile =
+                    profileQuerySnapshot.docs[0]?.data()?.name || "";
+
+                // Return the combined data
+                return {
+                    job,
+                    industry,
+                    jobType,
+                    profile,
+                };
+            } else {
+                console.error(
+                    "No matching document found in 'posts' collection."
+                );
+                return null;
+            }
+        } else {
+            console.error("No matching document found for jobCode:");
             return null;
         }
     } catch (error) {
@@ -55,41 +142,104 @@ async function fetchJobDocument(jobCode) {
     }
 }
 
-const jobPromise = fetchJobDocument(jobCode);
+// Fetch job data from local storage
+const job = fetchJobDataFromLocalStorage(jobCode);
 
-jobPromise.then((job) => {
-    if (!job) {
-        console.error("Job not found.");
-    } else {
-        const postNameElement = document.getElementById("postName");
-        if (postNameElement) {
-            postNameElement.textContent = job.post_name;
-        }
+if (job) {
+    console.log(job);
+    updateElementHTMLOrHide(
+        "industry",
+        job.industry ? `<strong>Industry:</strong> ${job.industry}` : ""
+    );
+    updateElementHTMLOrHide(
+        "jobType",
+        job.jobType ? `<strong>Job Type:</strong> ${job.jobType}` : ""
+    );
+    updateElementHTMLOrHide(
+        "profile",
+        job.profile ? `<strong>Profile:</strong> ${job.profile}` : ""
+    );
 
-        const briefDetailElement = document.getElementById("briefDetail");
-        if (briefDetailElement) {
-            briefDetailElement.innerHTML = `<strong>Brief Details:</strong> ${job.brief_info}`;
-        }
-
-        const dateElement = document.getElementById("date");
-        if (dateElement) {
-            dateElement.innerHTML = `<strong>Post Date:</strong> ${job.post_date} | <strong>Last Date:</strong> ${job.last_date}`;
-        }
-
-        const ageLimitElement = document.getElementById("ageLimit");
-        if (ageLimitElement) {
-            ageLimitElement.innerHTML = `<strong>Minimum Age Limit:</strong> ${job.minimum_age} | <strong>Maximum Age Limit:</strong> ${job.maximum_age}`;
-        }
-
-        const recruitmentBoardElement =
-            document.getElementById("recruitmentBoard");
-        if (recruitmentBoardElement) {
-            recruitmentBoardElement.innerHTML = `<strong>Recruitment Board:</strong> ${job.recruitment_board}`;
-        }
-
-        const moreDetailsLink = document.getElementById("moreDetailsLink");
-        if (moreDetailsLink) {
-            moreDetailsLink.href = job.job_link;
-        }
+    const postNameElement = document.getElementById("postName");
+    if (postNameElement) {
+        postNameElement.textContent = job.postName;
     }
-});
+
+    const briefDetailElement = document.getElementById("briefDetail");
+    if (briefDetailElement) {
+        briefDetailElement.innerHTML = `<strong>Brief Details:</strong> ${job.briefInfo}`;
+    }
+
+    const dateElement = document.getElementById("date");
+    if (dateElement) {
+        dateElement.innerHTML = `<strong>Post Date:</strong> ${job.postDate} | <strong>Last Date:</strong> ${job.lastDate}`;
+    }
+
+    const ageLimitElement = document.getElementById("ageLimit");
+    if (ageLimitElement) {
+        ageLimitElement.innerHTML = `<strong>Minimum Age Limit:</strong> ${job.minAge} | <strong>Maximum Age Limit:</strong> ${job.maxAge}`;
+    }
+
+    const recruitmentBoardElement = document.getElementById("recruitmentBoard");
+    if (recruitmentBoardElement) {
+        recruitmentBoardElement.innerHTML = `<strong>Recruitment Board:</strong> ${job.recruitmentBoard}`;
+    }
+
+    const moreDetailsLink = document.getElementById("moreDetailsLink");
+    if (moreDetailsLink) {
+        moreDetailsLink.href = job.jobLink;
+    }
+} else {
+    const jobPromise = fetchJobDocument(jobCode);
+
+    jobPromise.then((result) => {
+        const { job, industry, jobType, profile } = result;
+        if (!job) {
+            console.error("Job not found.");
+        } else {
+            updateElementHTMLOrHide(
+                "industry",
+                industry ? `<strong>Industry:</strong> ${industry}` : ""
+            );
+            updateElementHTMLOrHide(
+                "jobType",
+                jobType ? `<strong>Job Type:</strong> ${jobType}` : ""
+            );
+            updateElementHTMLOrHide(
+                "profile",
+                profile ? `<strong>Profile:</strong> ${profile}` : ""
+            );
+
+            const postNameElement = document.getElementById("postName");
+            if (postNameElement) {
+                postNameElement.textContent = job.post_name;
+            }
+
+            const briefDetailElement = document.getElementById("briefDetail");
+            if (briefDetailElement) {
+                briefDetailElement.innerHTML = `<strong>Brief Details:</strong> ${job.brief_info}`;
+            }
+
+            const dateElement = document.getElementById("date");
+            if (dateElement) {
+                dateElement.innerHTML = `<strong>Post Date:</strong> ${job.post_date} | <strong>Last Date:</strong> ${job.last_date}`;
+            }
+
+            const ageLimitElement = document.getElementById("ageLimit");
+            if (ageLimitElement) {
+                ageLimitElement.innerHTML = `<strong>Minimum Age Limit:</strong> ${job.minimum_age} | <strong>Maximum Age Limit:</strong> ${job.maximum_age}`;
+            }
+
+            const recruitmentBoardElement =
+                document.getElementById("recruitmentBoard");
+            if (recruitmentBoardElement) {
+                recruitmentBoardElement.innerHTML = `<strong>Recruitment Board:</strong> ${job.recruitment_board}`;
+            }
+
+            const moreDetailsLink = document.getElementById("moreDetailsLink");
+            if (moreDetailsLink) {
+                moreDetailsLink.href = job.job_link;
+            }
+        }
+    });
+}
--- js/script.js
+++ js/script.js
@@ -1,14 +1,146 @@
-window.addEventListener("scroll", () => {
-    let scrollVal = window.scrollY;
-    if (scrollVal > 2000) {
-        document.getElementById("backToTopBtn").style.display = "block";
-    } else {
-        document.getElementById("backToTopBtn").style.display = "none";
-    }
-});
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
+import {
+    getFirestore,
+    getDocs,
+    where,
+    collection,
+    query,
+} from "https://www.gstatic.com/firebasejs/10.2.0/firebase-firestore.js";
 
-function scrollToTop() {
-    document.documentElement.scrollTop = 0;
+const firebaseConfig = {
+    apiKey: "AIzaSyDzoJJ_325VL_axuuAFzDf3Bwt_ENzu2rM",
+    authDomain: "jobsdoor360-39b87.firebaseapp.com",
+    databaseURL: "https://jobsdoor360-39b87-default-rtdb.firebaseio.com",
+    projectId: "jobsdoor360-39b87",
+    storageBucket: "jobsdoor360-39b87.appspot.com",
+    messagingSenderId: "326416618185",
+    appId: "1:326416618185:web:de19e90fe4f06006ef3318",
+    measurementId: "G-60RHEMJNM6",
+};
+
+// Initialize Firebase app and Firestore
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+
+async function fetchJobDocument(postName) {
+    try {
+        if (postName) {
+            // Fetch the "posts" document using "post_name"
+            const postQuerySnapshot = await getDocs(
+                query(
+                    collection(db, "posts"),
+                    where("post_name", "==", postName)
+                )
+            );
+
+            if (!postQuerySnapshot.empty) {
+                const postData = postQuerySnapshot.docs[0].data();
+
+                // Fetch "industry_masterdata", "jobtype_masterdata", and "profile_masterdata"
+                const industryCode = postData.industry_masterdata_code;
+                const jobTypeCode = postData.jobtype_masterdata_code;
+                const profileCode = postData.profile_masterdata_code;
+
+                // Fetch data from "industry_masterdata"
+                const industryQuerySnapshot = await getDocs(
+                    query(
+                        collection(db, "industry_masterdata "),
+                        where("code", "==", industryCode)
+                    )
+                );
+
+                const industry =
+                    industryQuerySnapshot.docs[0]?.data()?.name || "";
+
+                // Fetch data from "jobtype_masterdata"
+                const jobTypeQuerySnapshot = await getDocs(
+                    query(
+                        collection(db, "jobtype_masterdata"),
+                        where("code", "==", jobTypeCode)
+                    )
+                );
+
+                const jobType =
+                    jobTypeQuerySnapshot.docs[0]?.data()?.name || "";
+
+                // Fetch data from "profile_masterdata"
+                const profileQuerySnapshot = await getDocs(
+                    query(
+                        collection(db, "profile_masterdata "),
+                        where("code", "==", profileCode)
+                    )
+                );
+
+                const profile =
+                    profileQuerySnapshot.docs[0]?.data()?.name || "";
+
+                // Return the combined data
+                return {
+                    industry,
+                    jobType,
+                    profile,
+                };
+            } else {
+                console.error(
+                    "No matching document found in 'posts' collection."
+                );
+                return null;
+            }
+        } else {
+            console.error("No matching document found for postName:");
+            return null;
+        }
+    } catch (error) {
+        console.error("Error fetching job document:", error);
+        return null;
+    }
 }
 
-document.getElementById("backToTopBtn").addEventListener("click", scrollToTop);
+// Function to fetch all jobs from Firestore
+async function fetchAllJobs() {
+    // Check if jobs data is available in local storage
+    const jobsDataFromLocalStorage = localStorage.getItem("jobsData");
+
+    if (jobsDataFromLocalStorage) {
+        // If data is available in local storage, parse it
+        JSON.parse(jobsDataFromLocalStorage);
+    } else {
+        // If data is not available in local storage, fetch it from Firestore
+        const jobCollectionRef = collection(db, "jobs");
+        const jobQuerySnapshot = await getDocs(jobCollectionRef);
+
+        const jobs = [];
+
+        for (const doc of jobQuerySnapshot.docs) {
+            const jobData = doc.data();
+            const job = {
+                postName: jobData.post_name,
+                qualificationEligibility: jobData.qualification_eligibility,
+                postDate: jobData.post_date,
+                lastDate: jobData.last_date,
+                jobCode: encodeURIComponent(jobData.job_code),
+                briefInfo: jobData.brief_info,
+                minAge: jobData.minimum_age,
+                maxAge: jobData.maximum_age,
+                recruitmentBoard: jobData.recruitment_board,
+                jobLink: jobData.job_link,
+            };
+
+            // Call fetchJobDocument for additional fields
+            const additionalData = await fetchJobDocument(jobData.post_name);
+
+            if (additionalData) {
+                job.industry = additionalData.industry;
+                job.jobType = additionalData.jobType;
+                job.profile = additionalData.profile;
+            }
+
+            jobs.push(job);
+        }
+
+        // Store jobs data in local storage
+        localStorage.setItem("jobsData", JSON.stringify(jobs));
+    }
+}
+
+fetchAllJobs();
