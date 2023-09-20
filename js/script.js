d2fed9203e546390dcf104330f5332e66b49a2a7
Jatin Kushwah <singhjatin354@gmail.com>
2023-09-20 08:55:16 +0530

    like-dislike strip added

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ import {
     where,
     collection,
     query,
+    doc,
+    getDoc,
+    setDoc,
+    addDoc,
 } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -21,6 +25,30 @@ const firebaseConfig = {
 // Initialize Firebase app and Firestore
 const app = initializeApp(firebaseConfig);
 const db = getFirestore();
+
+const modal = new bootstrap.Modal(document.getElementById("myModal"));
+const closeModal = document.getElementById("closeModal");
+const feedbackInput = document.getElementById("feedback");
+const phoneNumberInput = document.getElementById("phoneNumber");
+
+const submitButton = document.getElementById("submitFeedback");
+
+const likeButton = document.getElementById("like-button");
+const dislikeButton = document.getElementById("dislike-button");
+
+likeButton.addEventListener("click", () => {
+    likeButton.classList.add("clicked");
+    setTimeout(() => {
+        likeButton.classList.remove("clicked");
+    }, 600);
+});
+
+dislikeButton.addEventListener("click", () => {
+    dislikeButton.classList.add("clicked");
+    setTimeout(() => {
+        dislikeButton.classList.remove("clicked");
+    }, 600);
+});
 
 async function fetchJobDocument(postName) {
     try {
@@ -144,3 +172,75 @@ async function fetchAllJobs() {
 }
 
 fetchAllJobs();
+
+function openModal() {
+    modal.show();
+}
+
+async function updateFeedbackCounts(likeCount, dislikeCount) {
+    try {
+        const feedbackDocRef = doc(db, "user_feedback", "a5uhEqWYKfElgJ74R87m");
+        await setDoc(feedbackDocRef, {
+            like_count: likeCount,
+            dislike_count: dislikeCount,
+        });
+    } catch (error) {
+        console.error("Error updating feedback counts:", error);
+    }
+}
+
+document.getElementById("like-button").addEventListener("click", async () => {
+    const feedbackDocRef = doc(db, "user_feedback", "a5uhEqWYKfElgJ74R87m");
+    const feedbackDocSnapshot = await getDoc(feedbackDocRef);
+    const newLikeCount = (feedbackDocSnapshot.data().like_count || 0) + 1;
+    await updateFeedbackCounts(
+        newLikeCount,
+        feedbackDocSnapshot.data().dislike_count || 0
+    );
+});
+
+document
+    .getElementById("dislike-button")
+    .addEventListener("click", async () => {
+        openModal();
+        const feedbackDocRef = doc(db, "user_feedback", "a5uhEqWYKfElgJ74R87m");
+        const feedbackDocSnapshot = await getDoc(feedbackDocRef);
+        const newDislikeCount =
+            (feedbackDocSnapshot.data().dislike_count || 0) + 1;
+        await updateFeedbackCounts(
+            feedbackDocSnapshot.data().like_count || 0,
+            newDislikeCount
+        );
+    });
+
+submitButton.addEventListener("click", async () => {
+    const feedback = feedbackInput.value;
+    const phoneNumber = phoneNumberInput.value;
+
+    const feedbackData = {
+        phoneNumber: phoneNumber,
+        feedback: feedback,
+    };
+
+    try {
+        const feedbackCollectionRef = collection(db, "user_feedback");
+
+        // Add a new document with the feedback data
+        await addDoc(feedbackCollectionRef, feedbackData);
+
+        console.log("User feedback stored successfully.");
+    } catch (error) {
+        console.error("Error storing user feedback:", error);
+    }
+
+    // Close the modal
+    modal.hide();
+
+    // Clear input fields
+    feedbackInput.value = "";
+    phoneNumberInput.value = "";
+});
+
+closeModal.addEventListener("click", async () => {
+    modal.hide();
+});
