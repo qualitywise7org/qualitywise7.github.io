6cbd66dc161651fd873f7eb7d1619b02f9c1886e
ayushmeena1712 <ayush.meena.cs@gmail.com>
2024-02-01 23:46:34 +0530

    Database is connected everything is working well

--- yourprofile/js/script.js
+++ yourprofile/js/script.js
@@ -1,3 +1,26 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import {
+  getFirestore,
+  getDoc,
+  setDoc,
+  doc,
+  updateDoc,
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzoJJ_325VL_axuuAFzDf3Bwt_ENzu2rM",
+  authDomain: "jobsdoor360-39b87.firebaseapp.com",
+  databaseURL: "https://jobsdoor360-39b87-default-rtdb.firebaseio.com",
+  projectId: "jobsdoor360-39b87",
+  storageBucket: "jobsdoor360-39b87.appspot.com",
+  messagingSenderId: "326416618185",
+  appId: "1:326416618185:web:de19e90fe4f06006ef3318",
+  measurementId: "G-60RHEMJNM6",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 $(document).ready(function () {
     $('.repeater').repeater({
         initEmpty: false,
@@ -64,13 +87,23 @@ function collectFormData() {
     return formData;
 }
 
-function saveFormDataToFile() {
+function saveFormDataToDatabase() {
     var formData = collectFormData();
-    var jsonData = JSON.stringify(formData);
 
-    console.log(jsonData);
+    const formDataRef = doc(db, "formData", "formData");
+ 
+    setDoc(formDataRef, formData)
+      .then(() => {
+        console.log("Document successfully written!");
+      })
+      .catch((error) => {
+        console.error("Error writing document: ", error);
+      });
 }
 
 $('#btn').on('click', function () {
-    saveFormDataToFile();
+    saveFormDataToDatabase();
 });
+
+
+
