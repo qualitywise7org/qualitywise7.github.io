e34f87be08bed6417055ce1a04c0085d9a314878
qualitywise7 <129479242+qualitywise7@users.noreply.github.com>
2024-05-05 23:03:08 +0530

    added jd360-testing db config

--- staticfiles/db/dbconfig.js
+++ staticfiles/db/dbconfig.js
@@ -32,7 +32,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-storage.js";
 
 // Initialize Firebase app
-const firebaseConfig = {
+const firebaseConfig1 = {
     apiKey: "AIzaSyDzoJJ_325VL_axuuAFzDf3Bwt_ENzu2rM",
     authDomain: "jobsdoor360-39b87.firebaseapp.com",
     databaseURL: "https://jobsdoor360-39b87-default-rtdb.firebaseio.com",
@@ -42,6 +42,18 @@ const firebaseConfig = {
     appId: "1:326416618185:web:de19e90fe4f06006ef3318",
     measurementId: "G-60RHEMJNM6",
 };
+
+
+//js360-testing config
+const firebaseConfig = {
+    apiKey: "AIzaSyB50M8vFXcbkJ_SGNncFzzK0RHMLQpNIzU",
+    authDomain: "jd360-testing.firebaseapp.com",
+    projectId: "jd360-testing",
+    storageBucket: "jd360-testing.appspot.com",
+    messagingSenderId: "576496756828",
+    appId: "1:576496756828:web:4482903b8db23a66285e99",
+    measurementId: "G-GN8843MVDG"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
