e4c1533515f3a1d75db8c9153fcd1337c1e0f6d6
Lavanya Shrivastava <lavanya.jobsdoor360@gmail.com>
2024-06-25 19:04:06 +0530

    changes

--- staticfiles/mainfiles/apply/cv_upload/script.js
+++ staticfiles/mainfiles/apply/cv_upload/script.js
@@ -1,25 +1,25 @@
 // Ensure the Firebase SDKs are imported if not using the module bundler
 // Importing Firebase functions for storage and Firestore
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getFirestore, doc, updateDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+// import { getFirestore, doc, updateDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+// import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
 
 console.log("script.js loaded");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDzoJJ_325VL_axuuAFzDf3Bwt_ENzu2rM",
-  authDomain: "jobsdoor360-39b87.firebaseapp.com",
-  databaseURL: "https://jobsdoor360-39b87-default-rtdb.firebaseio.com",
-  projectId: "jobsdoor360-39b87",
-  storageBucket: "jobsdoor360-39b87.appspot.com",
-  messagingSenderId: "326416618185",
-  appId: "1:326416618185:web:de19e90fe4f06006ef3318",
-  measurementId: "G-60RHEMJNM6"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDzoJJ_325VL_axuuAFzDf3Bwt_ENzu2rM",
+//   authDomain: "jobsdoor360-39b87.firebaseapp.com",
+//   databaseURL: "https://jobsdoor360-39b87-default-rtdb.firebaseio.com",
+//   projectId: "jobsdoor360-39b87",
+//   storageBucket: "jobsdoor360-39b87.appspot.com",
+//   messagingSenderId: "326416618185",
+//   appId: "1:326416618185:web:de19e90fe4f06006ef3318",
+//   measurementId: "G-60RHEMJNM6"
+// };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const storage = getStorage(app);
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+// const storage = getStorage(app);
 
 const emailApply = localStorage.getItem("emailApply");
 let userEmail = '';
