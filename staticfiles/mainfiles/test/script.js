725e746bae6070c593d4b8fb8967c350c0985ce3
Akashsahu360 <akash.sahu.jobsdoor360@gmail.com>
2024-07-21 09:42:14 +0530

    new updates on assessment too;

--- staticfiles/mainfiles/test/script.js
+++ staticfiles/mainfiles/test/script.js
@@ -1,40 +1,45 @@
+// Get the authenticated user's ID
+let userId = null;
+let userData = {};
 
-  async function fetchAssessments() {
-    try {
-      console.log(`Fetching assessments`);
-      let querySnapshot = await getDocs(
-        (collection(db, "assessment"))
-      );
-        console.log(`Assessments found`);
-        // use for loop and show on html
-        displayCards(querySnapshot);
-
-    } catch (error) {
-      console.error("Failed to fetch questions:", error);
-      return null;
-    }
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    // User is not logged in, show alert and redirect to Home page
+    alert("Please log in to attempt an assessment");
+    window.location.href = "/";
   }
+});
 
-  function displayCards(assessments) {
-    const divCont = document.getElementById("assessment");
-
-      let content = [];
-        assessments.forEach(
-              (doc) => {
-                let assessment = doc.data();
-                content.push(`
-                    <li>
-                      <label>
-                        <p>${assessment.title}</p>
-                        <p>${assessment.desc}</p>
-                        <a href="/test/quiz/?quizcode=${doc.id}">Start</a>
-                      </label>
-                    </li>`
-                  );
-                });
-      divCont.innerHTML = `<ul> + ${content.join()} + </ul>`;
-      console.log(assessments);
+async function fetchAssessments() {
+  try {
+    console.log(`Fetching assessments`);
+    let querySnapshot = await getDocs(
+      (collection(db, "assessment"))
+    );
+    console.log(`Assessments found`);
+    // use for loop and show on html
+    displayCards(querySnapshot);
+  } catch (error) {
+    console.error("Failed to fetch questions:", error);
+    return null;
   }
+}
 
+function displayCards(assessments) {
+  const divCont = document.getElementById("assessment");
 
-  fetchAssessments();
+  let content = [];
+  assessments.forEach((doc) => {
+    let assessment = doc.data();
+    content.push(`
+      <div class="assessment-box">
+        <h2 class="assessment-title">${assessment.title}</h2>
+        <a class="btn-start" href="/test/quiz/?quizcode=${doc.id}">Start Assessment</a>
+      </div>
+    `);
+  });
+  divCont.innerHTML = content.join('');
+  console.log(assessments);
+}
+
+fetchAssessments();
