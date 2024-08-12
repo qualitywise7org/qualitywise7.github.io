08dcb7fda227d76613a9aeed18e0fc0157359ad3
Akashsahu360 <akash.sahu.jobsdoor360@gmail.com>
2024-08-12 19:05:39 +0530

    Added guideline popup in quiz page instead of test page

--- staticfiles/mainfiles/test/script.js
+++ staticfiles/mainfiles/test/script.js
@@ -1,6 +1,5 @@
 let userId = null;
 let userData = {};
-let isPopupOpen = false;
 
 onAuthStateChanged(auth, (user) => {
   if (!user) {
@@ -16,7 +15,7 @@ async function fetchAssessments() {
     console.log(`Assessments found`);
     displayCards(querySnapshot);
   } catch (error) {
-    console.error("Failed to fetch questions:", error);
+    console.error("Failed to fetch assessments:", error);
     return null;
   }
 }
@@ -31,57 +30,13 @@ function displayCards(assessments) {
       <div class="assessment-box">
         <h2 class="assessment-title">${assessment.title}</h2>
         <form action="/test/quiz/" method="get">
-          <button type="button" class="btn-start" data-popup-id="${doc.id}">Start Assessment</button>
           <input type="hidden" name="quizcode" value="${doc.id}">
+          <button type="submit" class="btn-start">Start Assessment</button>
         </form>
       </div>
     `);
   });
   divCont.innerHTML = content.join('');
-
-  document.querySelectorAll('.btn-start').forEach(button => {
-    button.addEventListener('click', (e) => openPopup(button.getAttribute('data-popup-id'), e.target.form));
-  });
-
-  console.log(assessments);
-}
-
-function openPopup(popupId, form) {
-  if (isPopupOpen) return;
-
-  let popupTemplate = document.getElementById("popup-template").cloneNode(true);
-  popupTemplate.style.display = 'block';
-  popupTemplate.id = `popup-${popupId}`;
-  document.body.appendChild(popupTemplate);
-
-  let quizCodeInput = popupTemplate.querySelector('input[name="quizcode"]');
-  quizCodeInput.value = popupId;
-
-  let agreeCheckbox = popupTemplate.querySelector('#agree-checkbox');
-  let submitButton = popupTemplate.querySelector('.btn-submit');
-
-  submitButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (agreeCheckbox.checked) {
-      form.submit();
-    } else {
-      alert("Please agree to all guidelines before starting the assessment.");
-    }
-  });
-
-  popupTemplate.querySelector('.btn-back').addEventListener('click', () => closePopup(popupTemplate));
-
-  document.getElementById('overlay').style.display = 'block';
-  popupTemplate.classList.add("open-popup");
-  document.body.classList.add("no-scroll");
-  isPopupOpen = true;
-}
-
-function closePopup(popup) {
-  document.querySelector('.open-popup').remove();
-  document.getElementById('overlay').style.display = 'none';
-  document.body.classList.remove("no-scroll");
-  isPopupOpen = false;
 }
 
 fetchAssessments();
