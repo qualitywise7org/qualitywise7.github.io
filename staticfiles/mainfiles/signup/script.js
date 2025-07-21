0bafbedc220d15337690444d268405c9be04576c
Qualitywise7 <129479242+qualitywise7@users.noreply.github.com>
2025-07-21 17:15:18 +0530

    db config removed

--- staticfiles/mainfiles/signup/script.js
+++ staticfiles/mainfiles/signup/script.js
@@ -1,35 +1,3 @@
-// ==== Firebase Setup ====
-import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  onAuthStateChanged,
-  createUserWithEmailAndPassword,
-  updateProfile,
-  sendEmailVerification,
-  GoogleAuthProvider,
-  signInWithPopup,
-} from "firebase/auth";
-import {
-  getFirestore,
-  doc,
-  setDoc,
-  getDoc,
-} from "firebase/firestore";
-
-// ==== Firebase Config ====
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
-};
-
-// ==== Initialize Firebase ====
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
 
 // ==== Auth Redirect ====
 onAuthStateChanged(auth, async (user) => {
