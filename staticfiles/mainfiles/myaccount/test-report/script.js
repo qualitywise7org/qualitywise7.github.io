08b77bdba9ed66c8693c411e3c998b4b9644d2aa
Qualitywise7 <129479242+qualitywise7@users.noreply.github.com>
2025-07-21 17:14:33 +0530

    remove db config from other places

--- staticfiles/mainfiles/myaccount/test-report/script.js
+++ staticfiles/mainfiles/myaccount/test-report/script.js
@@ -1,37 +1,3 @@
-import {
-  getAuth,
-  onAuthStateChanged,
-} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
-
-import {
-  getFirestore,
-  doc,
-  getDoc,
-  collection,
-  query,
-  where,
-  getDocs,
-} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDzoJJ_325VL_axuuAFzDf3Bwt_ENzu2rM",
-  authDomain: "jobsdoor360-39b87.firebaseapp.com",
-  databaseURL: "https://jobsdoor360-39b87-default-rtdb.firebaseio.com",
-  projectId: "jobsdoor360-39b87",
-  storageBucket: "jobsdoor360-39b87.appspot.com",
-  messagingSenderId: "326416618185",
-  appId: "1:326416618185:web:de19e90fe4f06006ef3318",
-  measurementId: "G-60RHEMJNM6",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
 let userId = null;
 
 document.addEventListener("DOMContentLoaded", () => {
